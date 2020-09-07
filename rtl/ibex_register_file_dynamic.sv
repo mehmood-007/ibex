6e15b0d4f509d676af4cc39bd1de7f2c2342375d
Mehmood Saleem <mehmood,fastian@gmail.com>
2020-09-07 14:20:39 +0200

    dynamic 4 RF

--- rtl/ibex_register_file_dynamic.sv
+++ rtl/ibex_register_file_dynamic.sv
@@ -77,9 +77,9 @@ module ibex_register_file #(
   logic [CACHE_LEN-1:0] cache_c_match_comb;
   logic write_enable;
 
-  logic [$clog2(CACHE_LEN)-1:0] tag_a;
-  logic [$clog2(CACHE_LEN)-1:0] tag_b;
-  logic [$clog2(CACHE_LEN)-1:0] tag_c;
+  wire [$clog2(CACHE_LEN)-1:0] tag_a [CACHE_LEN:0];
+  wire [$clog2(CACHE_LEN)-1:0] tag_b [CACHE_LEN:0];
+  wire [$clog2(CACHE_LEN)-1:0] tag_c [CACHE_LEN:0] ;
 
   logic [4:0] addr;
 
@@ -166,10 +166,10 @@ module ibex_register_file #(
       write_stall_1 <= 0;
     end
     else begin
-        if ( cache_miss_a )
+        if ( cache_miss_a_ )
             rf_reg_tmp[counter_a] <= l2_rdata_A;
-        else if ( |cache_c_match_comb )
-            rf_reg_tmp[tag_c] <= wdata_a_i;
+        else if ( write_stall_1 && |cache_c_match_comb)
+            rf_reg_tmp[tag_c[0]] <= wr_buf;
         write_stall_1 <= write_stall;
     end
   end
@@ -194,21 +194,21 @@ module ibex_register_file #(
 // Store data in buffers
   always_ff @(posedge clk_i or negedge rst_ni) begin: buffers
     if (!rst_ni) begin
-        rd_buf_a <= '{default:'0};
-        rd_valid_a <= 0;
-        rd_buf_b <= '{default:'0};
-        rd_valid_b <= 0;
+    //    rd_buf_a <= '{default:'0};
+    //    rd_valid_a <= 0;
+    //    rd_buf_b <= '{default:'0};
+    //    rd_valid_b <= 0;
         wr_buf <= '{default:'0};
         wr_valid <= 0;
     end else begin
-        rd_buf_a <= l2_rdata_A;// : rd_buf_a;
-        rd_buf_b <= l2_rdata_B;// : rd_buf_b;
-        rd_valid_a <= sel_op_a ? 1 : 
-                      pe || pe2 ? rd_valid_a : 
-                      0;
-        rd_valid_b <= sel_op_b ? 1 : 
-                      pe || pe2 ? rd_valid_b : 
-                      0;
+    //    rd_buf_a <= l2_rdata_A;// : rd_buf_a;
+    //    rd_buf_b <= l2_rdata_B;// : rd_buf_b;
+    //    rd_valid_a <= sel_op_a ? 1 : 
+    //                  pe || pe2 ? rd_valid_a : 
+    //                  0;
+    //    rd_valid_b <= sel_op_b ? 1 : 
+    //                 pe || pe2 ? rd_valid_b : 
+    //                  0;
         wr_buf  <= cache_miss_c ? wdata_a_i : wr_buf;
         wr_valid <= cache_miss_c ? 1 : 0;
     end
@@ -268,10 +268,9 @@ module ibex_register_file #(
       counter_a <= '{default:'0};  
      // counter_b <= '{default:'0};
      // counter_c <= '{default:'0};
-
     end else begin
       for (int r = 0; r < CACHE_LEN; r++) begin
-         // cache_1_index_1[r] <= cache_1_index[r];
+        cache_1_index_1[r] <= cache_1_index[r];
       end
 /*        if( |cache_a_match_comb == 0 && raddr_a_i != '0 && 
             |cache_b_match_comb == 0 && raddr_b_i != '0 && 
@@ -279,8 +278,8 @@ module ibex_register_file #(
           counter_a <= counter_a >= CACHE_LEN-1 ? 0 : counter_a + 1;
           counter_b <= counter_a >= CACHE_LEN-1 ? 0 : counter_a + 2;
         end
-        else*/ 
-        if( cache_a_match == 0 && raddr_a_i != '0 && !write_stall_1 ) begin
+        else*/
+        if( cache_miss_a_ ) begin
           counter_a <= counter_a >= CACHE_LEN-1 ? 0 : counter_a + 1;
          // counter_b <= counter_a >= CACHE_LEN-1 ? 0 : counter_a + 1;
         end
@@ -297,21 +296,45 @@ module ibex_register_file #(
     end
   end
   
+  assign tag_c[CACHE_LEN] = 2'b11;
+  assign tag_a[CACHE_LEN] = 2'b11;
+  assign tag_b[CACHE_LEN] = 2'b11;
   // The for-loop creates multiple assign statements
   genvar i;
   generate
     for ( i = 0; i < CACHE_LEN; i++ ) begin
       assign cache_a_match_comb[i] = (cache_1_index_1[i] == raddr_a_i && raddr_a_i != '0 && !write_stall_1 ) ? 1 : 0;
       assign cache_b_match_comb[i] = (cache_1_index_1[i] == raddr_b_i && raddr_b_i != '0 && !write_stall_1 ) ? 1 : 0;
-      assign cache_c_match_comb[i] = (cache_1_index_1[i] == waddr_a_i && write_enable) ? 1 : 0;
+      assign cache_c_match_comb[i] = (cache_1_index_1[i] == waddr && write_stall_1) ? 1 : 0;
 
-      assign tag_a = (cache_1_index_1[i] == raddr_a_i && raddr_a_i != '0) ? i : 0;
-      assign tag_b = (cache_1_index_1[i] == raddr_b_i && raddr_b_i != '0) ? i : 0;
-      assign tag_c = (cache_1_index_1[i] == waddr_a_i && write_enable) ? i : 0;
-
+//      assign tag_a[i] = (cache_1_index[i] == raddr_a_i && raddr_a_i != '0) ? i : tag_a[i+1];
+//      assign tag_b[i] = (cache_1_index[i] == raddr_b_i && raddr_b_i != '0) ? i : tag_b[i+1];
+//      assign tag_c[i] = (cache_1_index[i] == waddr) ? i : tag_c[i+1];
     end
   endgenerate
 
+  generate
+    for ( i = CACHE_LEN - 1; i >= 0 ; i-- ) begin
+      assign tag_a[i][1:0] = ~cache_a_match_comb[i] == 1'b0 ? i : tag_a[i+1][1:0];
+      assign tag_b[i][1:0] = ~cache_b_match_comb[i] == 1'b0 ? i : tag_b[i+1][1:0];
+      assign tag_c[i][1:0] = cache_c_match_comb[i] == 1'b1 ? i : tag_c[i+1][1:0];
+    end
+  endgenerate
+
+/*
+wire [15:0] _o_idx [0:$bits(occup_t)];
+assign _o_idx[$bits(occup_t)] = 'h1fff;
+
+genvar i;
+generate
+    for(i = $bits(occup_t)-1; i >= 0; i--)
+    begin
+        assign _o_idx[i][15:0] = 
+               _mp_occup[i] == 0 
+               ? i : _o_idx[i+1][15:0];
+    end
+endgenerate
+*/
 assign cache_a_match = |cache_a_match_comb;
 assign cache_b_match = new_inst && |cache_b_match_comb;
 assign cache_c_match = 0;//|cache_c_match_comb;
@@ -319,28 +342,23 @@ assign cache_c_match = 0;//|cache_c_match_comb;
 always @( * ) begin
   reg_address_a = '0;
   reg_address_b = '0;
-  temp_reg_a = '0;
-  temp_reg_b = '0;
+  temp_reg_a = l2_rdata_A;;
+  temp_reg_b = l2_rdata_B;;
   cache_miss_a = 0;
   cache_miss_b = 0;
   cache_miss_c = 0;
 
   if( cache_a_match == 1 )
-    temp_reg_a = rf_reg_tmp[tag_a];
- 
+    temp_reg_a = rf_reg_tmp[tag_a[0]];
   else if( cache_a_match == 0 && raddr_a_i != 0 && !write_stall_1) begin // doesn't exist in cache
-    temp_reg_a = rd_buf_a;
-    cache_1_index[counter_a] = raddr_a_i;
-   //  cache_1[counter_a] = temp_reg_a;
     cache_miss_a = 1;
-    // $display( "Cache miss, addr-A %d-> %d ", reg_address_a, counter_a );
-  end
-    
+     // cache_1[counter_a] = temp_reg_a;
+    //  $display( "Cache miss, addr-A %d-> %d ", reg_address_a, counter_a );
+  end 
 
   if( cache_b_match == 1 )
-    temp_reg_b =  rf_reg_tmp[tag_b];
+    temp_reg_b =  rf_reg_tmp[tag_b[0]];
   else if( cache_b_match == 0 && raddr_b_i != 5'h0 && !immediate_inst_i) begin // doesn't exist in cache
-    temp_reg_b = rd_buf_b;
     cache_miss_b = 1;
    // cache_1_index[counter_b] = raddr_b_i;
    // cache_1[counter_b] = temp_reg_b;
@@ -349,8 +367,8 @@ always @( * ) begin
    // counter = counter >= 1 ? 0 : counter + 1;
   // $display( "Cache miss, stall %d, addr %d ", reg_stall[reg_address_a], reg_address_a );
   end
-
-  cache_miss_c = cache_c_match == 0 && waddr_a_i != 0 && write_enable ? 1 : 0;
+  // cache_c_match == 0 &&
+  cache_miss_c = waddr_a_i != 0 && write_enable ? 1 : 0;
 
   if(cache_miss_c) begin
    // cache_1_index[counter_a] = waddr_a_i;
@@ -416,12 +434,12 @@ end
   // assign r_reg[NUM_WORDS-1:1] = _r_reg[NUM_WORDS-1:1];
  // assign r_reg[NUM_WORDS-1:0] = _r_reg[NUM_WORDS-1:0];
 // assign rdata_a_o =  raddr_a_i == 0 ? 0:
-  assign rdata_a_o =  raddr_a_i == 5'(0) ? rf_reg[0] : l2_rdata_A;//rd_buf_a;
-    //                  rd_valid_a ? rd_buf_a : 
+  assign rdata_a_o =  raddr_a_i == 5'(0) ? rf_reg[0] : temp_reg_a ;//rd_buf_a;
+      //                  rd_valid_a ? rd_buf_a : 
      //                 rf_reg_tmp[tag_a];
   
-  assign rdata_b_o =  raddr_b_i == 5'(0) ? rf_reg[0] : l2_rdata_B;//rd_buf_b;
- //                     rd_valid_b ? rd_buf_b : 
+  assign rdata_b_o =  raddr_b_i == 5'(0) ? rf_reg[0] : temp_reg_b;//rd_buf_b;
+   //                     rd_valid_b ? rd_buf_b : 
   //                    rf_reg_tmp[tag_b];
 
   assign sig = cache_miss_a || cache_miss_b;
@@ -440,8 +458,10 @@ end
    // Combinational logic where sig is AND with delayed, inverted version of sig
   // Assign statement assigns the evaluated expression in the RHS to the internal net pe
 	assign pe = sig & ~sig_dly;
-	assign pe2 = two_op_signal;
+	assign pe2 = 0;//two_op_signal;
 
+  assign cache_1_index[counter_a] = cache_a_match == 0 && raddr_a_i != 0 && !write_stall_1 && !pe? 
+                                    raddr_a_i : cache_1_index[counter_a];
 
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
