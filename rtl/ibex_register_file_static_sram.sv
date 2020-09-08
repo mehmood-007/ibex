1dcb5874e7f12ac5f9d239dde1df76f80c502a02
Mehmood Saleem <mehmood,fastian@gmail.com>
2020-09-09 01:00:24 +0200

    Static SRAM with reg size

--- rtl/ibex_register_file_static_sram.sv
+++ rtl/ibex_register_file_static_sram.sv
@@ -50,15 +50,21 @@ module ibex_register_file #(
   logic [31:0]  reg_stall;
   logic [31:0]  reg_stall_;
   
+  localparam int unsigned SIZE_REG = 4;
+
   localparam int unsigned ADDR_WIDTH = RV32E ? 4 : 5;
   localparam int unsigned NUM_WORDS  = 2**ADDR_WIDTH;
 
   logic [1:0][DataWidth-1:0] rf_reg;
-  logic [3:0][DataWidth-1:0] rf_reg_tmp;
+  logic [SIZE_REG-1:0][DataWidth-1:0] rf_reg_tmp;
   logic [NUM_WORDS-1:1] we_a_dec;
   logic reg_access;
 
   int idx_rd;
+
+  logic [DataWidth-1:0] temp_reg_a;
+  logic [DataWidth-1:0] temp_reg_b;
+
   logic cache_miss_a;
   logic cache_miss_b;
   logic cache_miss_c;
@@ -68,6 +74,11 @@ module ibex_register_file #(
   logic [DataWidth-1:0] wr_buf;
 
   logic [DataWidth-1:0] l2_rdata;
+
+  logic [DataWidth-1:0] l2_rdata_A;
+  logic [DataWidth-1:0] l2_rdata_B;  
+  
+  logic L1_sig_wr; // exist in level-1
 
   logic reg_stall_t;
   logic reg_stall_long; 
@@ -83,7 +94,7 @@ module ibex_register_file #(
   
   logic [4:0] tag;
   logic [4:0] addr;
-  logic L1_sig_wr; // exist in level-1
+
 
   logic sel_op_a;
   logic sel_op_b;
@@ -96,8 +107,18 @@ module ibex_register_file #(
 
   logic sel_sec_op;
   logic write_enable;
-
   logic cnt, temp;
+
+  logic [4:0] addrA;
+  logic [4:0] addrB;
+  wire [$clog2(SIZE_REG)-1:0] raddr_a;
+  wire [$clog2(SIZE_REG)-1:0] raddr_b;
+  wire [$clog2(SIZE_REG)-1:0] waddr_a;
+
+  logic new_inst;
+  logic [31:0] temp_pc_id;
+  logic [4:0] waddr;
+  logic shift_1, shift_2;
 
   always_comb begin : we_a_decoder
     for (int unsigned i = 1; i < NUM_WORDS; i++) begin
@@ -105,34 +126,31 @@ module ibex_register_file #(
     end
   end
 
-assign write_enable = |we_a_dec;
-
-logic new_inst;
-logic [31:0] temp_pc_id;
+  assign raddr_a = raddr_a_i[$clog2(SIZE_REG)-1:0];
+  assign raddr_b = raddr_b_i[$clog2(SIZE_REG)-1:0];
+  assign waddr_a = waddr_a_i[$clog2(SIZE_REG)-1:0];
 
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
         temp_pc_id <= '{default:'0};
     end
     else begin
-        temp_pc_id <= temp_pc_id != pc_id_i && !write_stall_patch ? pc_id_i : temp_pc_id; 
+        temp_pc_id <= temp_pc_id != pc_id_i && !write_stall_1 ? pc_id_i : temp_pc_id; 
     end
   end
 
-assign new_inst = temp_pc_id != pc_id_i && !write_stall_patch ? 1 : 0;
-
-logic [4:0] waddr;
+assign new_inst = temp_pc_id != pc_id_i && !write_stall_1 ? 1 : 0;
+assign write_enable = |we_a_dec;
 assign addr = wr_valid ? waddr :
               sel_op_a ? raddr_a_i :
               sel_op_b ? raddr_b_i : 0;
 
-always_ff @(posedge clk_i or negedge rst_ni) begin
-  if (!rst_ni) begin
-    sel_sec_op <= 0;
-  end else begin
-    sel_sec_op <= (cache_miss_a && cache_miss_b) ? 1 : 0;
-  end
-end
+  assign addrA = raddr_a_i;
+  // sel_op_b ?  : 0;
+  assign addrB = wr_valid ? waddr : raddr_b_i;
+  // sel_op_a ?
+  
+
 
 always_ff @(posedge clk_i or negedge rst_ni) begin
   if (!rst_ni) begin
@@ -144,71 +162,41 @@ end
 
 always_ff @(posedge clk_i or negedge rst_ni) begin: buffers
   if (!rst_ni) begin
-    rd_buf_a <= '{default:'0};
-    rd_valid_a <= 0;
-    rd_buf_b <= '{default:'0};
-    rd_valid_b <= 0;
     wr_buf <= '{default:'0};
     wr_valid <= 0;
   end else begin
-    rd_buf_a <= sel_op_a ? l2_rdata : rd_buf_a;
-    rd_buf_b <= sel_op_b ? l2_rdata : rd_buf_b;
-    rd_valid_a <= sel_op_a ? 1 : 
-                  pe || pe2 ? rd_valid_a : 
-                  0;
-    rd_valid_b <= sel_op_b ? 1 : 
-                  pe || pe2 ? rd_valid_b : 
-                  0;
     wr_buf  <= write_enable && !L1_sig_wr ? wdata_a_i : 0;
     wr_valid <= write_enable && !L1_sig_wr ? 1 : 0;
   end
 end
-
-assign tag = addr < 8 ? addr: 8 + (addr-15);
-//assign L1_sig_wr = ( waddr_a_i == 12 || waddr_a_i == 13 || waddr_a_i == 14 || waddr_a_i == 15 ) ? 1 : 0;
 
 always_ff @(posedge clk_i or negedge rst_ni) begin
   if (!rst_ni) begin
     rf_reg_tmp <= '{default:'0};
     write_stall_1 <= 0;
   end else begin
-    if ( write_enable )
-      rf_reg_tmp[waddr_a_i-12] <= L1_sig_wr ? wdata_a_i : rf_reg_tmp[waddr_a_i-12];
+    if ( write_enable && L1_sig_wr )
+      rf_reg_tmp[waddr_a] <= wdata_a_i;
   write_stall_1 <= write_stall;
   end
 end
 
-/*
- // L2 register access
- ibex_l2_register_file l2 (
-      .clk_i      (clk_i),
-      .rst_ni     (rst_ni),
-      .addr_i     (tag),
-      .wdata_i    (wr_buf),
-      .rdata_o    (l2_rdata),
-      .we_i       (wr_valid)
+  SRAM2RW32x32 l2_sram (
+      .A1(addrA),
+      .A2(addrB),
+      .CE1(clk_i),
+      .CE2(clk_i),
+      .WEB1(1'b1),
+      .WEB2(~wr_valid),
+      .OEB1(1'b0),
+      .OEB2(1'b0),
+      .CSB1(1'b0),
+      .CSB2(1'b0),
+      .I1(),
+      .I2(wr_buf),
+      .O1(l2_rdata_A),
+      .O2(l2_rdata_B)
   );
-*/
-logic oe;
-assign oe = 1'b0; 
- SRAM2RW32x32 l2_sram (
-     .A1(tag),
-     .A2(),
-     .CE1(clk_i),
-     .CE2(),
-     .WEB1(~wr_valid),
-     .WEB2(),
-     .OEB1(oe),
-     .OEB2(),
-     .CSB1(1'b0),
-     .CSB2(),
-     .I1(wr_buf),
-     .I2(),
-     .O1(l2_rdata),
-     .O2()
-);
-
- // r_reg_count[r] <= (raddr_a_i == 5'(r) || raddr_b_i == 5'(r)) ? (r_reg_count[r] + 1) : r_reg_count[r];
 
 always_comb begin
   cache_miss_a = 0;
@@ -216,24 +204,29 @@ always_comb begin
   cache_miss_c = 0;
   write_stall = 0;
   L1_sig_wr = 1;
-  if( raddr_a_i != 12 && raddr_a_i != 13 && raddr_a_i != 14 && raddr_a_i != 15 && !write_stall_patch  )
+  temp_reg_a = rf_reg_tmp[raddr_a];
+  temp_reg_b = rf_reg_tmp[raddr_b];
+  // && raddr_a_i != 13 && raddr_a_i != 14 && raddr_a_i != 15
+ // raddr_a_i[4:3] != 2'b01 
+  if( raddr_a_i[4:2] != 3'b011 && !write_stall_1  ) begin
     cache_miss_a = 1;
-  if( raddr_b_i != 12 && raddr_b_i != 13 && raddr_b_i != 14 && raddr_b_i != 15 && !write_stall_patch )
+    temp_reg_a = l2_rdata_A;
+  end
+  // && raddr_b_i != 13 && raddr_b_i != 14 && raddr_b_i != 15
+  if( raddr_b_i[4:2] != 3'b011 && !write_stall_1 ) begin
       cache_miss_b = 1;
-  if( waddr_a_i != 12 && waddr_a_i != 13 && waddr_a_i != 14 && waddr_a_i != 15  && write_enable ) begin
+      temp_reg_b = l2_rdata_B;
+  end
+  // && waddr_a_i != 13 && waddr_a_i != 14 && waddr_a_i != 15 
+  if( waddr_a_i[4:2] != 3'b011  && write_enable ) begin
       cache_miss_c = 1;
       L1_sig_wr = 0;
   end
+
   cache_miss_a = new_inst ? cache_miss_a : 0;
   cache_miss_b = new_inst && !immediate_inst_i ? cache_miss_b : 0;
   write_stall = cache_miss_c && !cnt ? 1 : 0;
 end
-
-assign sel_op_a = cache_miss_a && cache_miss_b ? 1 :
-                  cache_miss_a ? 1 : 0;
-
-assign sel_op_b = cache_miss_a && cache_miss_b ? 0 :
-                  cache_miss_b || sel_sec_op ? 1 : 0;
 
 always_ff @(posedge clk_i or negedge rst_ni) begin
   if (!rst_ni) begin
@@ -275,50 +268,29 @@ end
     assign rf_reg[0] = '0;
   end
 
-
-  assign rdata_a_o =  raddr_a_i == 5'(0) ? rf_reg[0] :
-                      rd_valid_a ? rd_buf_a : 
-                      rf_reg_tmp[raddr_a_i - 12];
-  
-  assign rdata_b_o =  raddr_b_i == 5'(0) ? rf_reg[0] :
-                      rd_valid_b ? rd_buf_b : 
-                      rf_reg_tmp[raddr_b_i - 12];
-
+  assign rdata_a_o = raddr_a_i == 5'(0) ? rf_reg[0] : temp_reg_a;
+  assign rdata_b_o = raddr_b_i == 5'(0) ? rf_reg[0] : temp_reg_b;
 
   assign sig = cache_miss_a || cache_miss_b;
     // This always block ensures that sig_dly is exactly 1 clock behind sig
   always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
+    if (!rst_ni)
       sig_dly <= 0;
-      two_op_signal <= 0;
-    end
-    else begin
+    else
       sig_dly <= sig;
-      two_op_signal <= cache_miss_a && cache_miss_b;
-    end
   end
 
  // Combinational logic where sig is AND with delayed, inverted version of sig
 // Assign statement assigns the evaluated expression in the RHS to the internal net pe
 assign pe = sig & ~sig_dly;
-assign pe2 = two_op_signal;
 
-logic shift_1, shift_2;
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      shift_1 <= 0;
-      shift_2 <= 0;
-    end
-    else begin
-    	shift_1 <= write_stall_1;
-      shift_2 <= shift_1;
-    end
-	end
+always_ff @(posedge clk_i or negedge rst_ni) begin
+  if (!rst_ni)
+    shift_1 <= 0;
+  else
+    shift_1 <= write_stall_1;
+end
 
-  assign write_stall_patch = write_stall_1;
-  assign write_stall_o = pe ? shift_1 : 
-                         pe2 ? shift_2 : write_stall_1;
-  assign reg_stall_o = pe || pe2 || write_stall_o ;
- // assign reg_access_o = reg_access;
-  
+assign write_stall_o = pe ? shift_1 : write_stall_1;
+assign reg_stall_o = pe || write_stall_o ;
 endmodule
